98655c4b2ceba041ad1785b266c07212be664e84
Marco Bertini <m.bertini92@gmail.com>
2022-07-04 12:09:35 +0200

    Implemented subroutine get_path_on_groups()

--- src/mod_scm_data.f90
+++ src/mod_scm_data.f90
@@ -2,13 +2,15 @@ module mod_scm_data
 
   use, intrinsic :: iso_fortran_env
   use mod_error
+  use mod_get_field
 
   implicit none
   save
   private
 
   ! public procedures
-  public :: set_scm_variables
+  public :: set_scm_variables,           &
+            get_path_on_groups
 
   ! protected variables
   protected :: flag_set_scm_variables,   &
@@ -105,6 +107,38 @@ subroutine set_scm_variables(t,labels,bars,pde,rde,flag_groups,groups)
   flag_set_scm_variables = .true.
 
 end subroutine set_scm_variables
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine get_path_on_groups(path,sout)
+
+  character(*), intent(in) :: path
+  character(*), intent(out) :: sout
+  character(*), parameter :: my_name = "get_path_on_groups"
+  character(20) :: field
+  character(8) :: istr
+  integer :: i
+  integer :: gi
+  integer :: tmp
+  integer :: err_n
+  character(120) :: err_msg
+
+  if (.not.flag_scm_groups) call error(my_name,"groups not setted")
+
+  sout = ""
+  tmp = -1
+  do i = 1, count_fields(path)
+    call get_field(path,field,i,err_n,err_msg) ! no need to check exit status
+    read(field,*) gi
+    if (allgroups(gi) /= tmp) then
+      tmp = allgroups(gi)
+      write(istr,'(I8)') tmp
+      istr = adjustl(istr)
+      sout = trim(sout)//" "//trim(istr)
+    end if
+  end do
+
+end subroutine get_path_on_groups
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
